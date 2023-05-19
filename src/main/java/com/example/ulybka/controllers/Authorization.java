2956518448e82d850adbbc7795d9f9db63590fd0
Sh1bari <vova_krasnov_2004@mail.ru>
2023-05-20 01:45:00 +0300

    auth

---
+++ src/main/java/com/example/ulybka/controllers/Authorization.java
@@ -0,0 +1,23 @@
+package com.example.ulybka.controllers;
+
+import com.example.ulybka.models.Admin;
+import com.example.ulybka.models.Answer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class Authorization {
+
+    @PostMapping("/checkAuth")
+    private Answer check(@RequestBody Admin admin){
+        Answer answer = new Answer("denied");
+        if(admin.getLogin().equals("ElenaTac") && admin.getPassword().equals("admin123")){
+            answer.setStatus("done");
+        }
+        return answer;
+    }
+}
---
+++ src/main/java/com/example/ulybka/models/Admin.java
@@ -0,0 +1,15 @@
+package com.example.ulybka.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Admin {
+    private String login;
+    private String password;
+}
