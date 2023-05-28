46928daf205a979720ba2a61404f47563ab1db95
Sh1bari <vova_krasnov_2004@mail.ru>
2023-05-28 16:59:10 +0300

    done

--- src/main/java/com/example/ulybka/entities/Comments.java
+++ src/main/java/com/example/ulybka/entities/Comments.java
@@ -16,7 +16,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class Comments {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
     private String name;
--- src/main/java/com/example/ulybka/entities/OurWork.java
+++ src/main/java/com/example/ulybka/entities/OurWork.java
@@ -11,7 +11,7 @@ import lombok.*;
 @ToString
 public class OurWork {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
     @Lob
